811f5fa9570770b070fb1cd5de2384f330ff2a9d
evogel10 <evogel@vt.edu>
2018-02-03 17:28:25 -0500

    Add files via upload

---
+++ kmean.r
@@ -0,0 +1,8 @@
+#Bring in tissue sample data
+geneData <- read.table("/Users/ericvogel/Documents/Johns_Hopkins/Principles_of_Bioinformatics_EN.605.451.81.FA17/Assignments/Module 10/assignment_10_dataset.txt", sep="\t", header=TRUE)
+
+#Manipulate the data so that the columns represent the genes for all the samples
+geneData <- t(geneData)
+
+#Use kmean function to group the data into 2 clusters
+tissueCluster <- kmeans(geneData[2:9, 2:11714], 2, nstart = 25)
