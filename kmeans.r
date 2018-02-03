525ef2b09eacc54ae51b650183a7b3f56ce85c83
evogel10 <evogel@vt.edu>
2018-02-03 17:35:18 -0500

    Update and rename kmean.r to kmeans.r

--- kmeans.r
+++ kmeans.r
@@ -1,3 +1,5 @@
+#Determines which samples are taken from which tissue by utilizing Kmeans Clustering
+
 #Bring in tissue sample data
 geneData <- read.table("/Users/ericvogel/Documents/Johns_Hopkins/Principles_of_Bioinformatics_EN.605.451.81.FA17/Assignments/Module 10/assignment_10_dataset.txt", sep="\t", header=TRUE)
 
